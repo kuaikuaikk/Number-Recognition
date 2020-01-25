1eaa370441285bbc025ff8770175fc1d0c6e0233
Chengkai Yao <yyyy7777kkkk@163.com>
2020-01-25 15:04:32 -0800

    add hello.cpp and hello.h

---
+++ hello.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yaock on 1/25/20.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+// Created by yaock on 1/25/20.
+//
+
+#ifndef NUMBER_RECOGNITION_HELLO_H
+#define NUMBER_RECOGNITION_HELLO_H
+
+
+class hello {
+
+};
+
+
+#endif //NUMBER_RECOGNITION_HELLO_H
